12f12f8449d64331f2793774ce3dea714812e3a3
Slavi Stoev <sl_stoev@abv.bg>
2019-08-18 19:37:09 +0300

    Initial Commit 2: Game Boogaloo Added big LightingData.assets to LFS. Doesn't matter, will be squashed.

---
+++ Assets/Editor/SettingsComponentEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(MonoBehaviour),true)]
+public class SettingsComponentEditor : Editor {
+
+    public override void OnInspectorGUI ()
+    {
+        DrawDefaultInspector();
+
+        if ( Application.isPlaying )
+        {
+            var type = target.GetType();
+
+            foreach ( var method in type.GetMethods(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance) )
+            {
+                var attributes = method.GetCustomAttributes(typeof(UpdateVariables), true);
+                if ( attributes.Length > 0 )
+                {
+                    if ( GUILayout.Button(method.Name) )
+                    {
+                        ((MonoBehaviour)target).Invoke(method.Name, 0f);
+                    }
+                }
+            }
+        }
+    }
+}
